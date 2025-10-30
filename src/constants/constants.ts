e18af88ef2ccaab2770f6eccebee8ed025d12349
NGUYỄN TRẦN PHÚ QUÝ <nguyentranphuquy@gmail.com>
2025-10-30 21:34:08 +0700

    test CI CD

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,9 @@
-export const PAGE_SIZE = 8;
+export const PAGE_SIZE = 5;
 
 export const LIGHT_MODE = "light",
 	DARK_MODE = "dark",
 	AUTO_MODE = "auto";
-export const DEFAULT_THEME = AUTO_MODE;
+export const DEFAULT_THEME = AUTO_MODEE;
 
 // Banner height unit: vh
 export const BANNER_HEIGHT = 35;
